a8b148b99333b3ccf08da147c49387f73206898f
kimbs95 <lifee456@naver.com>
2023-02-16 13:50:26 +0900

    2023-02-16 깃허브 시작!

---
+++ app/src/main/java/com/example/heejaetest/ch3/Exam01.kt
@@ -0,0 +1,12 @@
+package com.example.heejaetest.ch3
+
+import com.example.heejaetest.User
+import com.example.heejaetest.data
+import com.example.heejaetest.formatData
+import java.util.*
+
+fun main(){
+    data = 20
+    var saydate = formatData(Date())
+    println(saydate)
+}
